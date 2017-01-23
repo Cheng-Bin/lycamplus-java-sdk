9d8a8d544e2edd15ab15be827946e0dd25215251
ChengBin <9971556658@qq.com>
2017-01-23 16:46:55 +0800

    added gradle property

--- src/main/java/tv/lycam/config/Constant.java
+++ src/main/java/tv/lycam/config/Constant.java
@@ -5,6 +5,10 @@ package tv.lycam.config;
  */
 public class Constant {
 
+
+    public static final String VERSION = "1.0.0";
+
+
     public static final String HEADER_BEARER = "Bearer ";
 
     public static final String HEADER_AUTHORIZATION = "Authorization";
--- src/main/java/tv/lycam/utils/TimeUtil.java
+++ src/main/java/tv/lycam/utils/TimeUtil.java
@@ -1,7 +1,5 @@
 package tv.lycam.utils;
 
-import sun.util.resources.CalendarData;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
